e5ba2d3ff2a31f04304b63816732c29eb386f58a
L Carver <wifihum@gmail.com>
2026-04-17 23:04:52 -0700

    Overrides new and delete operators.

--- wk11_memory_allocation_tracking_sample.cpp
+++ wk11_memory_allocation_tracking_sample.cpp
@@ -2,6 +2,32 @@
 // wk11_memory_allocation_tracking_sample.cpp
 //------------------------------------------------------------------------------
 #include <iostream>
+#include <malloc.h>
+
+//------------------------------------------------------------------------------
+// override new operator to track memory allocations
+//      - malloc() is the memory allocation function in C, also works in C++
+//      - only use malloc() in C++ development code, not release code!
+//      - void* is generic memory pointer, you can cast to any pointer type
+//------------------------------------------------------------------------------
+void* operator new(size_t nBytes)
+{
+    std::cout << "Allocated " << nBytes << " bytes\n";
+
+    return malloc(nBytes);
+}
+
+//------------------------------------------------------------------------------
+// override delete operator to track memory deallocations
+//      - free() is the memory deallocation function in C, also works in C++
+//      - only use free() in C++ development code, not release code!
+//------------------------------------------------------------------------------
+void operator delete(void* pDelete, size_t nBytes)
+{
+    std::cout << "Deallocated " << nBytes << " bytes\n";
+
+    free(pDelete);
+}
 
 //------------------------------------------------------------------------------
 // entry point
